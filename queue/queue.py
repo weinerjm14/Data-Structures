b1cfb3eaabae81d7c2c52ba66ab1b92021538837
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-08-19 22:12:06 -0400

    queue as array passing

--- queue/queue.py
+++ queue/queue.py
@@ -13,16 +13,24 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+from collections import deque
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = deque([])
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        return self.storage.append(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.popleft()
